5c6bb0135d357203b9e53ab76c9c7b9d5ec25fc2
K00244549: Daniel Tze Yung Jong: Daniel Tze Yung Jong <K00244549@student.lit.ie>
2022-09-21 13:36:13 +0100

    adding simple functions to connect two activities

--- fundementals_1_2/app/src/main/java/com/example/fundementals_1_2/MainActivity.java
+++ fundementals_1_2/app/src/main/java/com/example/fundementals_1_2/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.fundementals_1_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,12 +13,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    public static final String EXTRA_MESSAGE = "com.example.fundementals_1_2.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = findViewById(R.id.show_count);
+
     }
 
     public void showToast(View view) {
@@ -29,5 +33,13 @@ public class MainActivity extends AppCompatActivity {
         if(mShowCount != null){
             mShowCount.setText(Integer.toString(mCount));
         }
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.tb_1);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
