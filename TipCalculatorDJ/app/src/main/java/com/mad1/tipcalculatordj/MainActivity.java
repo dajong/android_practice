16aef3925255cfc229b156f48b9c7e57c47ef823
K00244549: Daniel Tze Yung Jong: Daniel Tze Yung Jong <K00244549@student.lit.ie>
2022-09-22 11:45:34 +0100

    pushing p1 tipCalculator

---
+++ TipCalculatorDJ/app/src/main/java/com/mad1/tipcalculatordj/MainActivity.java
@@ -0,0 +1,73 @@
+package com.mad1.tipcalculatordj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText billAmountEditText;
+    private TextView percentTextView;
+    private Button  percentUpButton;
+    private Button percentDownButton;
+    private TextView tipTextView;
+    private TextView totalTextView;
+
+
+    private String billAmountString = "";
+    private float tipPercent = .15f;
+
+    public MainActivity() {
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculateAndDisplay() {
+
+        // get the bill amount
+        billAmountString = billAmountEditText.getText().toString();
+        float billAmount;
+        if (billAmountString.equals("")) {
+            billAmount = 0;
+        }
+        else {
+            billAmount = Float.parseFloat(billAmountString);
+        }
+
+        // calculate tip and total
+        float tipAmount = billAmount * tipPercent;
+        float totalAmount = billAmount + tipAmount;
+
+        // display the other results with formatting
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        tipTextView.setText(currency.format(tipAmount));
+        totalTextView.setText(currency.format(totalAmount));
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        percentTextView.setText(percent.format(tipPercent));
+    }
+
+
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+            calculateAndDisplay();
+        }
+        return false;
+    }
+
+
+}
