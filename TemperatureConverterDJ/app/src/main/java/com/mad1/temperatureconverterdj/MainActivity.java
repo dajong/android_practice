3fe788e926dff36e8e18cf8e2187fc850794c8c7
Daniel Jong <dajong@Daniels-MacBook-Air.local>
2022-09-29 19:56:43 +0100

    basic error handling

--- TemperatureConverterDJ/app/src/main/java/com/mad1/temperatureconverterdj/MainActivity.java
+++ TemperatureConverterDJ/app/src/main/java/com/mad1/temperatureconverterdj/MainActivity.java
@@ -2,13 +2,20 @@ package com.mad1.temperatureconverterdj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final DecimalFormat df = new DecimalFormat("0.00");
     private RadioButton toCelsius;
     private RadioButton toFahr;
     private EditText input;
@@ -22,16 +29,44 @@ public class MainActivity extends AppCompatActivity {
         toFahr = findViewById(R.id.toFahrRadioButton);
         input = findViewById(R.id.input);
         output = findViewById(R.id.answer);
+
+        input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_DONE ||
+                        actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+                    convert();
+                }
+                return false;
+            }
+        });
+    }
+
+    public void convert()
+    {
+        if(!input.getText().toString().matches("")){
+            float inputValue = Float.parseFloat(input.getText().toString());
+            if(toCelsius.isChecked()){
+                output.setText(df.format(ConverterUtils.convertFahrenheitToCelsius(inputValue)));
+            }else if(toFahr.isChecked()){
+                output.setText(df.format(ConverterUtils.convertCelsiusToFahrenheit(inputValue)));
+            }
+
+            BasicUtils.hideSoftKeyboard(this);
+        }
+
+        else{
+            Context context = getApplicationContext();
+            CharSequence error = "Please put in a valid value!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, error, duration);
+            toast.show();
+        }
+
     }
 
     public void convert(View view) {
-        float inputValue = Float.parseFloat(input.getText().toString());
-        if(toCelsius.isChecked()){
-            output.setText(Float.toString(ConverterUtils.convertFahrenheitToCelsius(inputValue)));
-        }else if(toFahr.isChecked()){
-            output.setText(Float.toString(ConverterUtils.convertCelsiusToFahrenheit(inputValue)));
-        }
-
-        BasicUtils.hideSoftKeyboard(this);
+        convert();
     }
 }
