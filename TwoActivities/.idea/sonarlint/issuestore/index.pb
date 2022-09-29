23b43e92e703881a6a2cc072446775b88b36995a
Daniel Jong <dajong@Daniels-MacBook-Air.local>
2022-09-29 18:40:14 +0100

    updating repo

---
+++ TwoActivities/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+m
+=app/src/main/java/com/example/twoactivities/MainActivity.java,a/d/ad8e3162ca54b66091f4ed8c5c17631d46b5350f
+p
+@app/src/main/java/com/example/twoactivities/activity_second.java,6/1/61d9a7b4986b7baf100096a1925e5089038bad01
--- TwoActivities/app/src/main/java/com/example/twoactivities/activity_second.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/activity_second.java
@@ -2,13 +2,75 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activity_second extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+    private static final String LOG_TAG = activity_second.class.getSimpleName();
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent =getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+        mReply = findViewById(R.id.editText_second);
+
+        Log.d(LOG_TAG, "------------");
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End " + LOG_TAG);
+        finish();
+        //startActivity(replyIntent);
     }
 }
