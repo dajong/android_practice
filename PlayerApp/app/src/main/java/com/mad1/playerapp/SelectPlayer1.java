26a767461973dff619fc711f64e6bdca65226bc9
K00244549: Daniel Tze Yung Jong <K00244549@student.lit.ie>
2022-11-16 12:14:36 +0000

    adding whatevers on the practical

---
+++ PlayerApp/app/src/main/java/com/mad1/playerapp/SelectPlayer1.java
@@ -0,0 +1,14 @@
+package com.mad1.playerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectPlayer1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_player1);
+    }
+}
